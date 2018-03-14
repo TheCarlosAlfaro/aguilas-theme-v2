ab922aa88e517aa4d5a57b3db8e7b847e2bf9140
devbysalas <rgkalin@gmail.com>
2018-03-14 09:17:17 -0700

    Working on home-remas and added post-types

--- parts/home-remas.php
+++ parts/home-remas.php
@@ -1,3 +1,28 @@
-<div id="home-remas" class="section">
-    <h1>This Is The home-remas section</h1>
+<div id="home-remas">
+    <div class="hidden-sx section split-color-back">
+        <h3 class="text-center section-header"><?php echo get_theme_mod('home_remas_heading', 'Remas'); ?></h3>
+        <p class="text-center section-sub-header"><?php echo get_theme_mod('home_remas_sub_heading', 'Enriqueze tu vida leendo los remas.'); ?></p>
+        <div class="container">
+            <div class="row">
+                <div class="col-md">
+                <?php
+                    $remaPosts = new WP_Query(array(
+                        'post_type' => 'rema',
+                        'posts_per_page' => 4
+                    ));
+                    while ($remaPosts->have_posts()) {
+                        $remaPosts->the_post(); ?>
+                        <li><?php the_title(); ?></li>
+                    <?php }
+                ?>
+
+                    <div class="text-center top-pad">
+                        <a href="<?php echo get_theme_mod('home_remas_button_url', '#'); ?>">
+                            <button type="button" class="btn btn-outline-primary"><?php echo get_theme_mod('home_remas_button_text', 'Ver todos'); ?></button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
