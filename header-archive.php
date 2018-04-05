a6fe5b4c2aad60c283d16ba2ce9d53fc0f1cf7f0
devbysalas <rgkalin@gmail.com>
2018-04-05 12:44:31 -0700

    Added header-archive.php for podcast and added content-podcast.php

---
+++ header-archive.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Aguilas
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?>>
+<div class="nav-container">
+
+	<?php $backgroundImg = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
+
+    <a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'aguilas' ); ?></a>
+
+    <header id="masthead" class="site-header" style="background:linear-gradient(0deg,rgba(0,0,0,0.1),rgba(0,0,0,0.1)), url('<?php echo $backgroundImg[0]; ?>') center no-repeat; background-size: 100%;">
+
+        <nav id="menu" class="navbar fixed-top navbar-expand-lg navbar-light" role="navigation">
+
+            <div class="container site-branding navbar-brand">
+                <?php
+                the_custom_logo();
+                if ( is_front_page() && is_home() ) : ?>
+                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                <?php else : ?>
+                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                <?php
+                endif;
+
+                $description = get_bloginfo( 'description', 'display' );
+                if ( $description || is_customize_preview() ) : ?>
+                    <p class="site-description"><?php echo $description; /* WPCS: xss ok. */ ?></p>
+                <?php
+                endif; ?>
+            </div><!-- .site-branding -->
+
+            <button class="navbar-toggler navbar-toggler-right custom-toggler" type="button" data-toggle="collapse" data-target="#bs4navbar" aria-controls="bs4navbar" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="menu-text-menu">MENU</span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <?php
+                wp_nav_menu([
+                    'menu'					=>	'primary',
+                    'theme_location'		=>	'primary',
+                    'container'				=>	'div',
+                    'container_id'			=>	'bs4navbar',
+                    'container_class'		=>	'collapse navbar-collapse',
+                    'menu_id'				=>	'main-menu',
+                    'menu_class' 			=>	'navbar-nav ml-auto',
+                    'depth'					=>	2,
+                    'fallback_cb'			=>	'bs4navwalker::fallback',
+                    'walker'				=>	new bs4navwalker()
+                ]);
+            ?>
+        </nav>
+        <div class="hero-content-single">
+			<h2 class="white hero-heading">Bienvenido Al <?php echo post_type_archive_title( '', false ); ?></h2>
+
+        </div>
+        <div class="social">
+		<p class="white hero-text">Comparte</p>
+            <a href="http://www.facebook.com/sharer/sharer.php?s=100&p[url]=<?php echo urlencode(get_permalink()); ?>" target="_blank">
+                <i class="fa fa-facebook"></i>
+            </a>
+            <a href="http://twitter.com/share?" target="_blank">
+                <i class="fa fa-twitter"></i>
+            </a>
+        </div>
+    </header><!-- #masthead -->
+</div>
---
+++ template-parts/content-podcast.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Aguilas
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header top-pad">
+
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3 col-lg-6">
+                <a href="<?php the_permalink(); ?>"><img class="card-img-top" src="<?php the_post_thumbnail_url(); ?>" alt="Card image cap"></a>
+            </div>
+            <div class="col-sm-8 col-lg-6">
+            <?php the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' ); ?>
+            <audio src="<?php the_field('audio_url'); ?>" controls="controls" style="width: 100%; height: 30px;"></audio>
+            </div>
+        </div>
+    </div>
+
+		<?php
+
+
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php aguilas_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			if (is_single()) {
+				the_content( sprintf(
+					wp_kses(
+						/* translators: %s: Name of current post. Only visible to screen readers */
+						__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'aguilas' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					get_the_title()
+				) );
+			} else {
+				the_excerpt();
+			}
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'aguilas' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php aguilas_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-<?php the_ID(); ?> -->
+<hr>
